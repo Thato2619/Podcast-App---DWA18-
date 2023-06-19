c6f52eca439df8068385641b6a1775b1638a70c2
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-20 01:43:04 +0200

    create history.js and import all the shows with historyGenre

---
+++ podcast/src/app/utils/assets/showGenres/history.js
@@ -0,0 +1,33 @@
+import '../show/5279';
+import '../show/9177';
+import '../show/6807';
+import '../show/8514';
+import '../show/5629';
+import '../show/8364';
+import '../show/5964';
+import '../show/9041';
+import '../show/5702';
+import '../show/5320';
+import '../show/6717';
+import '../show/5968';
+import '../show/8760'
+
+
+export const historyShows = [
+    '5279',
+    '9177',
+    '6807',
+    '8514',
+    '5629',
+    '8364',
+    '5964',
+    '9041',
+    '5702',
+    '5320',
+    '6717',
+    '5968',
+    '8760'
+]
+
+    
+
