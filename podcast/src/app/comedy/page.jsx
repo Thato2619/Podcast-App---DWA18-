ff49ab084d48ae2ddb50ae7204e531921c51fc05
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-20 00:23:27 +0200

    create a componets for nested routes

--- podcast/src/app/comedy/page.jsx
+++ podcast/src/app/comedy/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
+
 
 function Comedy() {
   return (
     <div>
-      This is comedy page
+       comedy page
+       <Link href='/shows'><h2>comedy Shows</h2></Link>
     </div>
   )
 }
---
+++ podcast/src/app/comedy/shows/pages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Shows() {
+  return (
+    <div>comedyShow</div>
+  )
+}
+
+export default Shows
