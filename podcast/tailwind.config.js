f8d32744fc920eb3efcbca36837545a5b690d677
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-19 14:40:08 +0200

    install material ui packages and add navbar from material ui

--- podcast/tailwind.config.js
+++ podcast/tailwind.config.js
@@ -1,4 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}); 
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
