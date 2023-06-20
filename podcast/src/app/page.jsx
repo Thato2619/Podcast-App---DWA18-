19c272978d8da0d65fe4333ff16092a618769fa8
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-20 10:37:48 +0200

    create category button component

--- podcast/src/app/page.jsx
+++ podcast/src/app/page.jsx
@@ -3,12 +3,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Header from '../components/Header'
+import GenreNav from '@/components/GenreNav'
 
 
 export default function Home() {
   return (
 <div>
   <Header />
+  <GenreNav />
   <h1>Please work</h1>
   <Link href="/comedy"><button>comedy</button></Link>
 </div>
--- podcast/src/components/GenreNav.jsx
+++ podcast/src/components/GenreNav.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function GenreNav() {
   return (
-    <div>
-      
+    <div classsName="mt-5 bg-[#202020] text-[#e9e2e7] text-sm flex flex-wrap justidy-between border border-l-0 border-r-0 border-t-[0.4px] border-b-[1px] border-[#a07e96] py-4 px-6">
+        <button className="bg-[#090708] hover:bg-[#a07e96] cursor-pointer border-2 border-[#a07e96] rounded-full text-center px-3 py-1 ">ALL</button>
     </div>
   )
 }
