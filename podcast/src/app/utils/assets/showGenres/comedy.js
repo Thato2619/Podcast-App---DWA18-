0d37ebbd8fd62cbaf87b025f3c795874b6084ce5
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-20 02:07:21 +0200

    create comedy and entertainment files and import all files from show folder

---
+++ podcast/src/app/utils/assets/showGenres/comedy.js
@@ -0,0 +1,6 @@
+import '../show/6807'
+
+export const comedyShows = [
+
+    '6807'
+]
---
+++ podcast/src/app/utils/assets/showGenres/entertainment.js
@@ -0,0 +1,16 @@
+import '../show/6807'
+import '../show/6631'
+import '../show/8256'
+import '../show/6756'
+import '../show/5702'
+import '../show/9620'
+import '../10758'
+export const entertainmentShows = [
+    "6807", 
+    "6631",
+    "8256", 
+    "6756", 
+    "5702",
+    "9620",
+    "10758"
+]
