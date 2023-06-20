f01d01bc6ab4bb3e693438888a5fb6aa90bbafbd
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-20 09:48:36 +0200

    import json files that historical and fictonal genres

---
+++ podcast/src/app/utils/assets/showGenres/business.js
@@ -0,0 +1,9 @@
+import '../show/8364'
+import '../6717'
+import '../show/8760'
+
+export const businessShows = [
+    "8364",
+    "6717", 
+    "8760"
+]
---
+++ podcast/src/app/utils/assets/showGenres/fiction.js
@@ -0,0 +1,19 @@
+import '../show/6631'
+import '../show/9664'
+import '../show/8188'
+import '../show/9702'
+import '../show/9695'
+import '../show/9994'
+import '../show/9263'
+import '../show/9666'
+
+export const fictionShows = [
+    "6631",
+    "9664",
+    "8188", 
+    "9702", 
+    "9695",
+    "9994",
+    "9263", 
+    "9666"
+    ]
