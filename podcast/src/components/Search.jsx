4a0477d4983a98d2394eae87d134454dd61104f6
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-21 16:13:23 +0200

    create logic of the functionality of the search bar

--- podcast/src/components/Search.jsx
+++ podcast/src/components/Search.jsx
@@ -2,12 +2,41 @@
 
 import React from 'react'
 import {useState} from 'react'
+import {SHOWS} from '../app/utils/assets/genre/genres'
+import SearchIcon from '@mui/icons-material/Search';
 
+
+const getFilteredItems = (query, items) => {
+    if(!query){
+        return undefined
+    }
+        return items.filter(title => title.name.includes(query))
+}
 function Search() {
-    const [query, setquery] = useState("")
+    const [query, setQuery] = useState("") 
+    
+    const {title} = SHOWS
+    const {items} = title
+
+    const filterItems = getFilteredItems(query, items)
+
   return (
     <div>
-      
+       <div className='flex items-center'>
+              <div className='flex w-[600px] items-center bg-[#090708]'>
+                  <input 
+                  type="text"
+                   placeholder='search' 
+                   className='bg-[#090708] flex-1 h-1-'
+                   onChange={e => setQuery(e.target.value)}/>
+                    {filterItems.map((value) => 
+                        <div key={value.id}>{value.title}</div>
+                    )}
+                    <button className='h-10 w-26 flex items-center justify-center hover:bg-[#fefefe]'>
+                      <SearchIcon className='h-5' />
+                  </button>
+              </div>
+          </div>
     </div>
   )
 }
